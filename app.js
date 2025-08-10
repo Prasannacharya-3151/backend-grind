29e78c634fb71c09efc68b75b71b28a2f42b8f23
Prasannacharya-3151 <prasannacharya428@gmail.com>
2025-08-10 19:33:52 +0530

    second commit

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ const app = express()
 const PORT = 3000;
 
 app.use(express.json());
+
+app.get ("/",(req,res)=>{
+    res.json({
+        message: "welcome to the first API"
+    })
+})
+
+app.get("/users", (req, res)=>{
+    res.json([
+        {id:1, name:"suhas"},
+        {id:2, name:"kirini"}
+    ]
+    )}
+)
