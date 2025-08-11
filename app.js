7d66510ff1f15ad81097e08b48273e2b92bc9356
Prasannacharya-3151 <prasannacharya428@gmail.com>
2025-08-11 21:36:28 +0530

    app.js

--- app.js
+++ app.js
@@ -31,3 +31,25 @@ app.post("/users", (req,res)=>{
 // })
 
 app.listen(3000);
+
+
+
+
+// const input  = [1, 2, 3, 4, 5];
+
+// constArray = [];
+
+// for (i=0; i<=input.length; i++){
+//     newArray.push(input[i]*2)
+// }
+// console.log(newArray);
+
+
+// const input = [1, 2, 3 , 4 ,5]
+
+// function transform(i){
+//     return i * 2
+// }
+
+// const ans = input.map(transform)
+// console.log(ans)
