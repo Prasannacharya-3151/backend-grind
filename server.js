34f4929d8954d80a3e7502b0f6dcf9e574ed81e3
Prasannacharya-3151 <prasannacharya428@gmail.com>
2025-08-12 16:22:02 +0530

    server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+
+const app = express()
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) =>{
+    res.json({
+        message:"welcome to my first API"
+    })
+})
+
+app.post("/", (req, res)=>{
+    const userData = req.body;
+    res.json({
+        message: "user data recieved",
+        data : userData
+    })
+})
+
+
+app.listen(PORT, () =>{
+    console.log(`server is running on http://localhost:${PORT}`)
+})
