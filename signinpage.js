89fe684f61bf11d39f0b23724edfb0a23c3e9582
Prasannacharya-3151 <prasannacharya428@gmail.com>
2025-08-13 22:37:13 +0530

    signup

--- signinpage.js
+++ signinpage.js
@@ -4,15 +4,50 @@ const app = express();
 
 app.use(express.json());
 
+const users = [];
+
+function generateToken(){
+    return Math.random()
+}
+
 app.post("/signup", function (req, res){
-    res.json({
-        msg:"you have signed up"
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username : username,
+        password : password
     })
+
+    res.json({
+        msg: "you are signed up"
+    })
+
 })
 
 app.post("/signin", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
 
+    let foundUser = null;
 
+    for (let i = 0; i<= users.length; i==){
+        if(users[i].username == username && users[i].password == password){
+            foundUser = users[i]
+        }
+    }
+
+    if (foundUser){
+        const token = generateToken();
+        foundUser.token = token;
+        res.json({
+            token: token
+        })
+    } else {
+        res.status(403).send({
+            mess: "Invalid username or password"
+        })
+    }
 })
 
 app.listen(3000, function(){
