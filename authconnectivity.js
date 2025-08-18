02144bd3e831b1bf770cd5e7a66c9d1fe928cea0
Prasannacharya-3151 <prasannacharya428@gmail.com>
2025-08-18 22:36:17 +0530

    auth

--- authconnectivity.js
+++ authconnectivity.js
@@ -1,7 +1,12 @@
-const express = reqired("express")
+const express = require("express")
 const jwt = require("jsonwebtoken")
+const JWT_SECRET = "harikirat123";
 
 const app = express();
+
+app.use(express.json());
+
+const users = [];
 
 app.post("/signup", function(req, res){
     const username = req.body.username;
@@ -12,16 +17,63 @@ app.post("/signup", function(req, res){
         password: password
     })
 
-    res.jsn({
+    res.json({
         msg: "you are signed up"
     })
 })
 
 app.post("/signin", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
 
+    let foundUser = null;
+
+    for(let i = 0; i < users.length; i++){
+        if (users[i].username == username && users[i].password == password) {
+            foundUser = users[i]
+        }
+    }
+
+    if(!foundUser) {
+        res.json({
+            msg:"Credential are mismatch"
+        })
+        return
+    } else {
+        
+        const token = jwt.sign({
+            username
+        }, JWT_SECRET);
+
+        res.json({
+            token: token
+        })
+    }
 
 })
 
 app.get("/me", function(req, res) {
+    const token = req.header.token;
 
+    const decodedData = jwt.verify(token, JWT_SECRET)
+
+    // const decodedData = jwt.decode(token)
+    if (decodedData.username) {
+        let foundUser = null;
+
+        for(let i = 0; i < users.length; i++){
+            if (users[i].username == decodedData.username) {
+                foundUser = users[i];
+            }
+        }
+
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        })
+    }
+})
+
+app.listen(3000, function(){
+    console.log("server started on port 3000")
 })
